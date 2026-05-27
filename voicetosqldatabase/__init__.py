addc6780c78013a153f055ab4defd8799b44fa8f
SyntaxSorcerer63 <202401244@daiict.ac.in>
2026-05-27 13:18:35 +0530

    Expose in-process entrypoints in __init__.py, add voicetosqldatabase package alias, and create pyproject.toml

---
+++ voicetosqldatabase/__init__.py
@@ -0,0 +1,4 @@
+"""Alias package for vtsql to allow importing directly as voicetosqldatabase."""
+
+from vtsql import *
+from vtsql import __all__
--- vtsql/__init__.py
+++ vtsql/__init__.py
@@ -1,1 +1,13 @@
 """Voice-to-SQL app package."""
+
+from vtsql.services.pipeline_service import run_nl_query, run_interpret, run_query
+from vtsql.client import VoiceToSQLClient
+from vtsql.audio_core import transcribe_upload
+
+__all__ = [
+    "run_nl_query",
+    "run_interpret",
+    "run_query",
+    "VoiceToSQLClient",
+    "transcribe_upload",
+]
